84aa9ce85ec9ff40190d06004d4604d7e5bce2a1
Juan José Peralta <juanjose@indicius.com>
2024-03-11 11:27:08 -0300

    feat(navbar): WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Navbar } from "@/components/Navbar";
+import { Container } from "@chakra-ui/react";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container maxW={"container.lg"}>
+      <Navbar />
+    </Container>
+  );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Logo } from "./Logo";
+import { Button, Avatar, HStack, Box, IconButton } from "@chakra-ui/react";
+import { AddIcon, HamburgerIcon, BellIcon } from "@chakra-ui/icons";
+
+export const Navbar = () => {
+  return (
+    <HStack w={"full"}>
+      <HStack spacing={12}>
+        <Logo />
+        <Button variant="ghost" leftIcon={<AddIcon />}>
+          Agregar peliculas
+        </Button>
+      </HStack>
+      <HStack ml={"auto"}>
+        <IconButton
+          aria-label="Menu"
+          variant="ghost"
+          icon={<HamburgerIcon />}
+        />
+        <IconButton
+          aria-label="Notification"
+          variant="ghost"
+          icon={<BellIcon />}
+        />
+        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+      </HStack>
+    </HStack>
+  );
+};
