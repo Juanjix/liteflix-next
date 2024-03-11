5cfa356e4b338c67a0b5ee12fe3ccd6751124546
Juan José Peralta <juanjose@indicius.com>
2024-03-11 14:03:17 -0300

    feat(navbar): add drawer menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,81 @@
 "use client";
 
+import { useRef } from "react";
+
 import { Logo } from "./Logo";
-import { Button, Avatar, HStack, IconButton } from "@chakra-ui/react";
+import {
+  Button,
+  Avatar,
+  HStack,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Stack,
+} from "@chakra-ui/react";
 import { AddIcon, HamburgerIcon, BellIcon } from "@chakra-ui/icons";
 
+const BurgerMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+  return (
+    <>
+      <IconButton
+        aria-label="Menu"
+        variant="ghost"
+        icon={<HamburgerIcon />}
+        ref={btnRef}
+        onClick={onOpen}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            <HStack justify={"space-between"}>
+              <DrawerCloseButton
+                top={"auto"}
+                right={"auto"}
+                position={"relative"}
+              />
+              <HStack>
+                <IconButton
+                  aria-label="Notifications"
+                  variant="ghost"
+                  icon={<BellIcon />}
+                />
+                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+              </HStack>
+            </HStack>
+          </DrawerHeader>
+          <DrawerBody>
+            <Stack>
+              <Button>Inicio</Button>
+              <Button>Series</Button>
+              <Button>Peliculas</Button>
+              <Button>Agregadas recientemente</Button>
+              <Button>Populares</Button>
+              <Button>Mis peliculas</Button>
+              <Button>Mi lista</Button>
+              <Button variant="ghost" leftIcon={<AddIcon />}>
+                Agregar peliculas
+              </Button>
+              <Button>Cerrar sesion</Button>
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 export const Navbar = () => {
   return (
     <HStack w={"full"}>
@@ -14,11 +86,7 @@ export const Navbar = () => {
         </Button>
       </HStack>
       <HStack ml={"auto"}>
-        <IconButton
-          aria-label="Menu"
-          variant="ghost"
-          icon={<HamburgerIcon />}
-        />
+        <BurgerMenu />
         <IconButton
           aria-label="Notifications"
           variant="ghost"
