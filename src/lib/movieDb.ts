586d50413be8d887cb467452af1cb33c63c3937a
Juan José Peralta <juanjose@indicius.com>
2024-03-20 14:03:57 -0300

    feat(env): add api_keys to env

--- src/lib/movieDb.ts
+++ src/lib/movieDb.ts
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 export async function getMovies() {
   const res = await fetch(
-    "https://api.themoviedb.org/3/movie/popular?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   );
 
   if (!res.ok) {
@@ -11,7 +15,7 @@ export async function getMovies() {
 
 export async function getNowPlaying() {
   const res = await fetch(
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   );
 
   const data = await res.json();
